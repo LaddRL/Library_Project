bdf3d3304b4a991bae00fa95e9f28415e24884fd
ladd lindsay <laddrlindsay@gmail.com>
2023-03-26 20:00:02 -0700

    committing for now

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-console.log("Hello World!\n==========\n");
+
 
 // PROJECT Section
-console.log("PROJECT:\n==========\n");
+
+const books = [{
+    title: "Name of the wind";
+    author: "Patrick Rothfuss";
+    read: true;
+}]
+
+class Book {
+    constructor (title, author, read) {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+} 
+
+class Library {
+    constructor (bookCount, books) {
+        this.bookCount = books.length;
+        this.books = books;
+    }
+    markRead (checkbox, id) {
+        this.books.forEach(books => {
+            if (id === books.id) {
+                books.read = true;
+                checkbox.checked = true;
+                checkbox.disabled = true;
+            }
+        });
+    }
+}
